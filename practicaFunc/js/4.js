5e55d1470966f1902a0c6832336f4b6fa3f07afb
Franco Casas <casassfranco@gmail.com>
2020-05-28 11:42:55 -0300

    Ejercicios de funciones terminados

--- practicaFunc/js/4.js
+++ practicaFunc/js/4.js
@@ -1,3 +1,17 @@
 /**
  * 4- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
  */
+
+function TablaDeMultiplicar(numero) {
+  let resultado = 0;
+  for (let i = 1; i <= 10; i++) {
+    resultado = i * numero;
+
+    document.write(`<br> ${numero}x${i} = ${resultado}`);
+    resultado == 0;
+  }
+}
+
+let numero = parseInt(prompt("Ingrese un numero", ""));
+
+TablaDeMultiplicar(numero);
